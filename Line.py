302ef1d8ed7cf1d9ef35d30dfbd7d0e93aea0726
Magic01Man <38139787+magic01man01@users.noreply.github.com>
2018-07-20 02:26:04 +0500

    develop arguman
    add output arguman

--- Line.py
+++ Line.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import sys
-
 os.system('clear')
 print """
 
@@ -30,51 +29,70 @@ parser.add_argument('-L' , action = "store" , dest= 'location', required = True 
 parser.add_argument('-B' , action = "store" , dest = 'Big' , type = int , required = False, help = "for biger than you'r number   > ")
 parser.add_argument('-S' , action = "store" , dest = 'Short' , type = int,required = False, help =  "for short than you'r number  <" )
 parser.add_argument('-E' , action = "store" , dest = 'Equal' , type = int , required = False, help = "for equal you'r number  =")
-parser.add_argument('-O' , action = "store" , dest = 'Output' , type = int , required = True, help = "Location Output")
+parser.add_argument('-O' , action = "store" , dest= 'output', required = True , help = "for you'r location's output ") 
 
 give_args = parser.parse_args() 
 location = give_args.location
 Big = give_args.Big
 Short = give_args.Short
 Equal = give_args.Equal
-Out = give_args.Output
+out = give_args.output
 
 
-with open(location) as file:
-    line = file.read().splitlines()
+try:
+    with open(location) as file:
+        line = file.read().splitlines()
+except IOError:
+    print("Please enter you'r location's file")
+    sys.exit()
 
-def equal():
+def equal(filename):
     number = 0
     for show in line:
         if len(show) == Equal:
             number+=1
+            try:
+                f = open(filename , 'a')
+            except IOError:
+                print("you should enter name's you'r text file in location's output")
+                sys.exit()
             print('{} : {}' . format(number , show))
-            f = open('Out' , 'a')
             f.write(show + '\n')
 
-def big():
+def big(filename):
     number = 0
     for show in line:
         if len(show) > Big:
             number+=1
+            try:
+                f = open(filename , 'a')
+            except IOError:
+                print("you should enter name's you'r text file in location's output")
+                sys.exit()
             print('{} : {}' . format(number , show))
-            f = open('Out' , 'a')
             f.write(show + '\n')
             
-def short():
+def short(filename):
     number = 0
     for show in line:
         if len(show) < Short:
             number+=1
+            try:
+                f = open(filename , 'a')
+            except IOError:
+                print("you should enter name's you'r text file in location's output")
+                sys.exit()
             print('{} : {}' . format(number , show))
-            f = open('Out' , 'a')
+            sys.exit()
             f.write(show + '\n')
 
+
 if Equal:
-    equal()
+    equal(out)
+
 
 if Big:
-    big()
+    big(out)
 
 if Short:
-    short()
+    short(out)
